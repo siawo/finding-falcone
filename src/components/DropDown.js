f68e114a253fde96255c8839f42077aefd7ab59c
siawo <md.owais1994@gmail.com>
2020-04-02 22:18:40 +0530

    created dropdown menu

--- src/components/DropDown.js
+++ src/components/DropDown.js
@@ -1,1 +1,13 @@
-export default () => {};
+import React from 'react';
+export default (props) => {
+    let { planets = [],
+        planetSelected = () => { },
+        index = 0,
+        resource = {},
+        selectedPlanetList = new Set() } = props,
+        nonSelectedPlanets = planets.filter(planet => !selectedPlanetList.has(planet.name) || resource.planet === planet.name);
+    return (<select defaultValue={resource.planet} onChange={e => { planetSelected(e.target.value, index) }}>
+        <option key='select'>Select</option>
+        {nonSelectedPlanets.map(planet => (<option key={planet.name}>{planet.name}</option>))}
+    </select>)
+};
