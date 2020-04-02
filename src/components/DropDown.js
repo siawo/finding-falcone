1aa7291264d097cb4dc8fe48ebda78768bff711e
siawo <md.owais1994@gmail.com>
2020-04-02 23:50:11 +0530

    formatted file

--- src/components/DropDown.js
+++ src/components/DropDown.js
@@ -4,10 +4,19 @@ export default (props) => {
         planetSelected = () => { },
         index = 0,
         resource = {},
-        selectedPlanetList = new Set() } = props,
-        nonSelectedPlanets = planets.filter(planet => !selectedPlanetList.has(planet.name) || resource.planet === planet.name);
-    return (<select defaultValue={resource.planet} onChange={e => { planetSelected(e.target.value, index) }}>
-        <option key='select'>Select</option>
-        {nonSelectedPlanets.map(planet => (<option key={planet.name}>{planet.name}</option>))}
-    </select>)
+        selectedPlanetList = new Set()
+    } = props,
+        nonSelectedPlanets = planets.filter(
+            planet => !selectedPlanetList.has(planet.name) || resource.planet === planet.name
+        );
+    return (
+        <select defaultValue={resource.planet} onChange={e => { planetSelected(e.target.value, index) }}>
+            <option key='select'>Select</option>
+            {
+                nonSelectedPlanets.map(
+                    planet => (<option key={planet.name}>{planet.name}</option>)
+                )
+            }
+        </select>
+    );
 };
