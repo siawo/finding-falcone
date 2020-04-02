3c1d60283c66c11acd948b3e9a903142321c42e7
siawo <md.owais1994@gmail.com>
2020-04-02 22:19:19 +0530

    Created logic to update dropdown based on selection

--- src/App.js
+++ src/App.js
@@ -1,41 +1,79 @@
 import React, { Component } from 'react';
 import './App.css';
-// import DropDwon from './components/DropDown.js';
+import DropDwon from './components/DropDown.js';
 import axios from 'axios';
 
 class App extends Component {
-  constructor (props) {
+  constructor(props) {
     super(props);
     this.state = {
       error: false,
       vehicles: [],
       planets: [],
+      selectedPlanetList: new Set(),
+      resourceAllocation: new Array(4).fill().map(() => ({})),
+      resourceRestriction: 4,
       totalTime: 0
     }
+    this.planetSelected = (planet, selectIndex) => {
+      this.setState(state => {
+        let { resourceAllocation,
+          selectedPlanetList } = state;
+        selectedPlanetList.delete(resourceAllocation[selectIndex].planet);
+        if (planet === 'Select') {
+          Object.assign(resourceAllocation[selectIndex], {
+            planet: undefined
+          });
+        } else {
+          Object.assign(resourceAllocation[selectIndex], {
+            planet
+          });
+          selectedPlanetList.add(planet);
+        }
+        return {
+          resourceAllocation,
+          selectedPlanetList
+        }
+      })
+    }
   }
-  componentDidMount () {
+  componentDidMount() {
     axios.all([
       axios.get('https://findfalcone.herokuapp.com/planets'),
       axios.get('https://findfalcone.herokuapp.com/vehicles')
     ])
-    .then(([planetsRes, vehiclesRes]) => {
-      this.setState({
-        vehicles: vehiclesRes.data,
-        planets: planetsRes.data
+      .then(([planetsRes, vehiclesRes]) => {
+        this.setState({
+          vehicles: vehiclesRes.data,
+          planets: planetsRes.data
+        })
       })
-    })
-    .catch(() => {
-      this.setState({
-        error: true
-      });
-    })
+      .catch(() => {
+        this.setState({
+          error: true
+        });
+      })
   }
-  render () {
-    let { error } = this.state;
+  render() {
+    let { error,
+      resourceAllocation,
+      planets,
+      selectedPlanetList } = this.state;
     return error ? (<h1> connect to Internet</h1>) : (
       <div className="App">
         <h1>Finding Falcon</h1>
         <p>Select planets you want to search in:</p>
+        {
+          resourceAllocation.map((res, i) =>
+            (<DropDwon
+              resource={res}
+              planetSelected={this.planetSelected}
+              key={i}
+              index={i}
+              planets={planets}
+              selectedPlanetList={selectedPlanetList}
+            />))
+        }
       </div>
     );
   }
